fed0d162de8b80c83bbde9925563986d95f1d5f1
shaqstradamus <vases@clarkson.edu>
2015-10-13 06:46:06 +0530

    Update Stage.java

--- core/src/com/manic/game/Stage.java
+++ core/src/com/manic/game/Stage.java
@@ -5,5 +5,19 @@
 package com.manic.game;
 
 public class Stage {
+  
+   /* public Stage (Viewport viewport)
+  
+   draw()
+
+   public void resize(int width, int height) {
+      viewport.update(width, height);
+   }
+
+   public void create() {
+    stage = new Stage(new StretchViewport(width, height));
+   }  
+
+*/
 
 }
