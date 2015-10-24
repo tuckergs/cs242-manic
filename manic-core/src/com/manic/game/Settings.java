e9d1bc32528f15d8f10126066ae0877c5fa528b2
Stephen Lorenz <Lorenzsj@clarkson.edu>
2015-10-24 12:23:32 -0400

    Updated Entity constructor and mutators

---
+++ manic-core/src/com/manic/game/Settings.java
@@ -0,0 +1,17 @@
+/*
+ * Description: Handles in-game settings
+ */
+
+package com.manic.game;
+
+public class Settings {
+	
+	//load()
+	
+	//save()
+	
+	//toggleSound()
+	
+	//.. etc
+
+}
---
+++ manic-core/src/com/manic/game/Stage.java
@@ -0,0 +1,23 @@
+/*
+ * Description: Handles creating the current world/map
+ */
+
+package com.manic.game;
+
+public class Stage {
+  
+   /* public Stage (Viewport viewport)
+  
+   draw()
+
+   public void resize(int width, int height) {
+      viewport.update(width, height);
+   }
+
+   public void create() {
+    stage = new Stage(new StretchViewport(width, height));
+   }  
+
+*/
+
+}
