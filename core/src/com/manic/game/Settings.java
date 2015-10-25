9eb6b9da8b208784b0f7dfca25981206e84958e0
Stephen Lorenz <Lorenzsj@clarkson.edu>
2015-10-25 14:46:12 -0400

    Added basic collision

--- core/src/com/manic/game/Settings.java
+++ core/src/com/manic/game/Settings.java
@@ -5,6 +5,9 @@
 package com.manic.game;
 
 public class Settings {
+	//box2D variables
+	//pixels per meter
+	public static final float PPM = 100;
 	
 	//load()
 	
--- core/src/com/manic/game/Start.java
+++ core/src/com/manic/game/Start.java
@@ -1,22 +1,72 @@
 package com.manic.game;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+//pixels per meter
+import static com.manic.game.Settings.PPM;
 
 public class Start extends GameState {
-	
-	private BitmapFont font = new BitmapFont();
+	private World world;
+	private final float X_GRAVITY = 0;
+	private final float Y_GRAVITY = -9.81f;
+	private Box2DDebugRenderer debugRenderer;
+	private OrthographicCamera box2DCamera;
 	
 	public Start(GameStateManager gsm) {
 		super(gsm);
+		
+		world = new World(new Vector2(X_GRAVITY, Y_GRAVITY), true);
+		debugRenderer = new Box2DDebugRenderer();
+		BodyDef bodyDef= new BodyDef();
+		FixtureDef fixtureDef = new FixtureDef();
+		
+		//create platform
+		bodyDef.position.set(160/PPM, 120/PPM);
+		bodyDef.type = BodyType.StaticBody; //unaffected by gravity
+		Body body = world.createBody(bodyDef);
+		
+		PolygonShape box = new PolygonShape();
+		box.setAsBox(50/PPM, 5/PPM); //100x10
+
+		fixtureDef.shape = box;
+		body.createFixture(fixtureDef);
+		
+		//create guy
+		bodyDef.position.set(160/PPM, 200/PPM);
+		bodyDef.type = BodyType.DynamicBody;
+		body = world.createBody(bodyDef);
+		
+		box.setAsBox(5/PPM, 5/PPM); //10x10
+		fixtureDef.shape = box;
+		body.createFixture(fixtureDef);
+		
+		//setup box2DCamera
+		box2DCamera = new OrthographicCamera();
+		box2DCamera.setToOrtho(false, Manic.V_WIDTH/PPM, Manic.V_HEIGHT/PPM);
 	}
 	
 	public void handleInput() {}
-	public void update(float dt) {}
+	
+	public void update(float dt)
+	{
+		world.step(dt, 6, 2);
+	}
 	public void render() {
-		sb.setProjectionMatrix(camera.combined);
-		sb.begin();
-		font.draw(sb, "play state", 100, 100);
-		sb.end();
+		//clear
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		//draw
+		debugRenderer.render(world, box2DCamera.combined);
 	}
 	
 	public void dispose() {}
