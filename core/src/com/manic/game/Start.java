c742667518001e9f95cbb04e3eef9cd0cc893a33
Stephen Lorenz <Lorenzsj@clarkson.edu>
2015-10-25 20:01:11 -0400

    Shaq can now play basketball when you get bored developing

--- core/src/com/manic/game/Start.java
+++ core/src/com/manic/game/Start.java
@@ -19,8 +19,11 @@ import static com.manic.game.Settings.PPM;
 
 public class Start extends GameState {
 	private World world;
-	private final float X_GRAVITY = 0;
-	private final float Y_GRAVITY = -2.81f;
+	
+	private final float GRAVITY_X = 0;
+	private final float GRAVITY_Y = -2.81f;
+	//private final float JUMP_FORCE_NEWTONS = 125;
+	private final float JUMP_FORCE_NEWTONS = 180;
 	private Box2DDebugRenderer debugRenderer;
 	private OrthographicCamera box2DCamera;
 	private MyContactListener contactListener;
@@ -29,7 +32,7 @@ public class Start extends GameState {
 	public Start(GameStateManager gsm) {
 		super(gsm);
 		
-		world = new World(new Vector2(X_GRAVITY, Y_GRAVITY), true);
+		world = new World(new Vector2(GRAVITY_X, GRAVITY_Y), true);
 		contactListener = new MyContactListener();
 		world.setContactListener(contactListener);
 		
@@ -50,16 +53,93 @@ public class Start extends GameState {
 		fixtureDef.filter.maskBits = Settings.BIT_PLAYER | Settings.BIT_BALL; //it can collide with both the player and ball
 		body.createFixture(fixtureDef).setUserData("platform");
 		
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//test platforms
+		//bottom plat
+		bodyDef.position.set(0/PPM, 0/PPM);
+		body = world.createBody(bodyDef);
+		box.setAsBox(320/PPM, 2/PPM); //100x10
+		body.createFixture(fixtureDef).setUserData("platform");
+		
+		//left side plat
+		bodyDef.position.set(0/PPM, 0/PPM);
+		body = world.createBody(bodyDef);
+		box.setAsBox(2/PPM, 240/PPM); //100x10
+		body.createFixture(fixtureDef).setUserData("platform");
+		
+		//right side plat
+		bodyDef.position.set(320/PPM, 0/PPM);
+		body = world.createBody(bodyDef);
+		box.setAsBox(2/PPM, 240/PPM); //100x10
+		body.createFixture(fixtureDef).setUserData("platform");
+		
+		bodyDef.position.set(0/PPM, 240/PPM);
+		body = world.createBody(bodyDef);
+		box.setAsBox(320/PPM, 2/PPM); //100x10
+		body.createFixture(fixtureDef).setUserData("platform");
+		
+		//shaqs badass practice hoop of true greatness. this is how he practices in real life
+		
+		bodyDef.position.set(300/PPM, 0/PPM);
+		body = world.createBody(bodyDef);
+		box.setAsBox(4/PPM, 155/PPM); //100x10
+		body.createFixture(fixtureDef).setUserData("platform");
+		
+		fixtureDef.filter.maskBits = Settings.BIT_PLAYER;
+		bodyDef.position.set(300/PPM, 180/PPM);
+		body = world.createBody(bodyDef);
+		box.setAsBox(25/PPM, 25/PPM); //100x10
+		body.createFixture(fixtureDef).setUserData("platform");
+		
+		bodyDef.position.set(300/PPM, 180/PPM);
+		body = world.createBody(bodyDef);
+		box.setAsBox(25/PPM, 25/PPM); //100x10
+		body.createFixture(fixtureDef).setUserData("platform");
+
+		bodyDef.position.set(300/PPM, 165/PPM);
+		body = world.createBody(bodyDef);
+		box.setAsBox(8/PPM, 8/PPM); //100x10
+		body.createFixture(fixtureDef).setUserData("platform");
+		
+		//loads of balls
+		CircleShape circle = new CircleShape();
+		circle.setRadius(5/PPM);
+		fixtureDef.shape = circle;
+		fixtureDef.restitution = 1.0f; //max bounce
+		fixtureDef.filter.categoryBits = Settings.BIT_BALL; //it is a type ball
+		fixtureDef.filter.maskBits = Settings.BIT_PLATFORM | Settings.BIT_PLAYER; //can collide with ground
+		bodyDef.position.set(153/PPM, 220/PPM);
+		bodyDef.type = BodyType.DynamicBody;
+		body = world.createBody(bodyDef);
+		bodyDef.position.set(10/PPM, 100/PPM);
+		
+		body = world.createBody(bodyDef);
+		body.createFixture(fixtureDef).setUserData("ball");
+		
+		bodyDef.position.set(20/PPM, 100/PPM);
+		body = world.createBody(bodyDef);
+		body.createFixture(fixtureDef).setUserData("ball");
+		
+		bodyDef.position.set(40/PPM, 100/PPM);
+		body = world.createBody(bodyDef);
+		body.createFixture(fixtureDef).setUserData("ball");
+		
+		bodyDef.position.set(60/PPM, 100/PPM);
+		body = world.createBody(bodyDef);
+		body.createFixture(fixtureDef).setUserData("ball");
+		
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		
 		//create ball guy
 		bodyDef.position.set(153/PPM, 220/PPM);
 		bodyDef.type = BodyType.DynamicBody;
 		body = world.createBody(bodyDef);
 		
-		CircleShape circle = new CircleShape();
+		//CircleShape circle = new CircleShape();
 		circle.setRadius(5/PPM);
 		fixtureDef.shape = circle;
-		fixtureDef.restitution = 1.0f;
+		fixtureDef.restitution = 1.0f; //max bounce
 		fixtureDef.filter.categoryBits = Settings.BIT_BALL; //it is a type ball
 		fixtureDef.filter.maskBits = Settings.BIT_PLATFORM | Settings.BIT_PLAYER; //can collide with ground
 		body.createFixture(fixtureDef).setUserData("ball");
@@ -70,13 +150,13 @@ public class Start extends GameState {
 		
 		box.setAsBox(5/PPM, 5/PPM); //10x10
 		fixtureDef.shape = box;
-		fixtureDef.restitution = 0.0f;
+		fixtureDef.restitution = 0.2f;
 		fixtureDef.filter.categoryBits = Settings.BIT_PLAYER;
 		fixtureDef.filter.maskBits = Settings.BIT_PLATFORM | Settings.BIT_BALL;
 		playerBody.createFixture(fixtureDef).setUserData("player");
 		
 		//create foot sensor
-		box.setAsBox(2/PPM, 2/PPM, new Vector2(0, -5/PPM), 0); //TODO GET RID OF MAGIC NUMBERS
+		box.setAsBox(5/PPM, 2/PPM, new Vector2(0, -5/PPM), 0); //TODO GET RID OF MAGIC NUMBERS
 		fixtureDef.shape = box;
 		fixtureDef.filter.categoryBits = Settings.BIT_PLAYER;
 		fixtureDef.filter.maskBits = Settings.BIT_PLATFORM;
@@ -95,8 +175,18 @@ public class Start extends GameState {
 		{
 			if (contactListener.isOnGround()) {
 				//in newtons. player weighs 1kg, -9.78 gravity
-				playerBody.applyForceToCenter(0, 125, true);
+				playerBody.applyForceToCenter(0, JUMP_FORCE_NEWTONS, true);
 			}
+		}
+		
+		if (InputHandler.isDown(InputHandler.KEY_D))
+		{
+			playerBody.applyForce(new Vector2(3f, 0), playerBody.getPosition(), true);
+		}
+		
+		if (InputHandler.isDown(InputHandler.KEY_A))
+		{
+			playerBody.applyForce(new Vector2(-3f, 0), playerBody.getPosition(), true);
 		}
 	}
 	
