ccafbe1e33ba2c8ddf9592784d552b1eb62f51ea
Dialox <091robinson@gmail.com>
2015-11-04 18:02:01 -0500

    Revert "Revert "Added Input Key S""
    This reverts commit e009a79a47ec277b3209d8b2893599552d7a8d14.

--- core/src/com/manic/game/InputProcessor.java
+++ core/src/com/manic/game/InputProcessor.java
@@ -23,6 +23,10 @@ public class InputProcessor extends InputAdapter
 			InputHandler.setKey(InputHandler.KEY_D, true); 
 		}
 		
+		if (key == Keys.S) {
+			
+			InputHandler.setKey(InputHandler.KEY_S, true); 
+		}
 		
 		return true;
 	}
@@ -42,6 +46,11 @@ public class InputProcessor extends InputAdapter
 		if (key == Keys.D) {
 		
 			InputHandler.setKey(InputHandler.KEY_D, false); 
+		}
+		
+		if (key == Keys.S) {
+			
+			InputHandler.setKey(InputHandler.KEY_S, false); 
 		}
 		
 		return true;
