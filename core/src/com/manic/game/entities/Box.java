8bfab23ccda3fd4d0908bb1deb45dc4d77f481b6
tuckergs <tuckergs@clarkson.edu>
2015-11-15 20:11:38 -0500

    Added some organization
    I put things in different packages
    This will be useful later

---
+++ core/src/com/manic/game/entities/Box.java
@@ -0,0 +1,66 @@
+package com.manic.game.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+import static com.manic.game.Settings.PPM;
+
+public class Box extends GameEntity {
+	protected float height = 0;
+    protected float width = 0;
+    
+    public Box(BodyType type, Vector2 coordinates, PolygonShape box, float height, float width, SpriteBatch batch, String spritePath)
+    {
+    	super(type, coordinates, batch, spritePath);
+    	
+    	setHeight(height);
+     	setWidth(width);
+
+     	bodyDef.position.set(coordinates);
+     	
+    	box.setAsBox(this.width, this.height);
+    	fixtureDef.shape = box;
+    }    
+    
+    //Getters
+    //Dimensions
+    public float getHeight()
+    {
+        return height;
+    }
+
+    public float getWidth()
+    {
+        return width;
+    }
+    
+    //Setters
+    //Dimensions
+    protected void setHeight(float height)
+    {
+        if (height > 0) {
+            this.height = height/PPM;
+        }
+        else {
+        	this.height = 1;
+        }
+    }
+    
+    protected void setWidth(float width)
+    {
+        if (width > 0) {
+            this.width = width/PPM;
+        }
+        else {
+        	this.width = 1;
+        }
+    }
+    
+    @Override
+    public String toString()
+    {
+    	return "";
+    }
+}
---
+++ core/src/com/manic/game/states/GameStateManager.java
@@ -0,0 +1,69 @@
+package com.manic.game.states;
+
+import java.util.Stack;
+
+import com.manic.game.Manic;
+
+public class GameStateManager {
+	private Manic manic;
+	private Stack<GameState> gameStates;
+	private enum States
+	{
+		PLAY, ROUND, EXIT
+	}
+	
+	public GameStateManager(Manic manic)
+	{
+		this.manic = manic;
+		gameStates = new Stack<GameState>();
+		
+		addState(States.PLAY);
+	}
+	
+	public Manic getManic()
+	{
+		return manic;
+	}
+	
+	private GameState getState(States state)
+	{
+ 		if (state == States.PLAY)
+ 		{
+ 			return new Start(this);
+ 		}
+ 		else
+ 		{
+ 			return null;
+ 		}
+	}
+	
+	public void setState(States state)
+	{
+		deleteState();
+		addState(state);
+		
+	}
+	
+	//add to top of stack
+	public void addState(States state)
+	{
+		gameStates.push(getState(state));
+	}
+	
+	//remove top of stack
+	public void deleteState() 
+	{
+		GameState state = gameStates.pop();
+		state.dispose();
+	}
+	
+	public void update(float dt)
+	{
+		gameStates.peek().update(dt);
+	}
+	
+	public void render()
+	{
+		gameStates.peek().render();
+	}
+}
