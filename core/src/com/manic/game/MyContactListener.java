b2726c92d22c2ed814dec79a1c474bd12c494f21
tuckergs <tuckergs@clarkson.edu>
2015-12-02 07:03:02 -0500

    Fixed commit problem
    I had to merge, and I forgot to fix a file

--- core/src/com/manic/game/MyContactListener.java
+++ core/src/com/manic/game/MyContactListener.java
@@ -22,12 +22,10 @@ public class MyContactListener implements ContactListener {
 	private FixtureDestroyer fixtureDestroyer;
 	
 	
-<<<<<<< HEAD
-	public void bindState ( GameState st ){
-=======
+
 	public void bindState ( Start st )
 	{
->>>>>>> origin/master
+
 		
 		state = st;
 		
